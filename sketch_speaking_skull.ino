9e4720d82e789a274a0ba91f0f07d282765add6e
AndSpi <andietat@gmail.com>
2020-10-25 17:45:40 +0100

    getting bored with the interrupts

--- sketch_speaking_skull.ino
+++ sketch_speaking_skull.ino
@@ -1,6 +1,6 @@
 // Partially based on https://makersportal.com/blog/2019/5/27/arduino-interrupts-with-pir-motion-detector
 
-volatile int state = LOW;
+volatile byte state = LOW;
 const int interrupt_pin = 2;
 int movement = LOW;
 const int photoresistor = A0;
@@ -8,7 +8,7 @@ const int led = 6;
 const int play = 12;
 int lum = 1;
 int brightness = 81;
-int treshold = 220;    // Level of light below which the eyes will trigger: 20 is a good default
+int treshold = 240;    // Level of light below which the eyes will trigger: 20 is a good default
 int dur = 2;  // Duration of flicker
 
 void setup() {
@@ -16,9 +16,10 @@ void setup() {
   pinMode(play, OUTPUT);
   digitalWrite(interrupt_pin, HIGH);
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(interrupt_pin),detection,RISING);
   delay(3000);
   digitalWrite(led, LOW);
+  
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin),detection,RISING);
 }
 
 
@@ -58,12 +59,13 @@ void loop() {
 
 
 void loop() {
+  
   if (state == HIGH){
+    
     lum = analogRead(photoresistor);
     Serial.print("Lum: ");
     Serial.println(lum);
     if (lum < treshold){
-      Serial.println("Gotcha!");
       flicker();
       }
     digitalWrite(led, LOW);
@@ -96,6 +98,7 @@ void flicker(){
 }
 
 void detection(){
-  flicker();
+  Serial.println("Gotcha!");
+  state = HIGH; 
 
 }
